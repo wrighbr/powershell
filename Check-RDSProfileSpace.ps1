112ef65d7d637bf7d159b24756339fcfd79d9b6a
Brett Wright <wrightb@AWAD05.assetware.com.au>
2017-02-14 14:02:07 +1100

    First Commit

---
+++ Check-RDSProfileSpace.ps1
@@ -0,0 +1,39 @@
+﻿$Volumes = Get-WmiObject win32_volume | Where-Object {$_.Label -eq 'User Disk'} # | fl *
+$Disks = @()
+$Nagios = 0
+$WarningLevel = 15
+$CriticalLevel = 5
+$Notify = 'OK'
+
+$Volumes | ForEach-Object {
+
+    $Object = New-Object PSObject -Property @{
+    
+    Name            = $_.name
+    'Capacity(GB)'  = [math]::Round($_.Capacity/'1073741824',2)
+    'FreeSpace(GB)' = [math]::Round($_.Freespace/'1073741824',2)
+    'Free(%)'       = [math]::Round($_.Freespace/$_.Capacity*'100',0)
+    
+    }
+    
+    $Disks += $Object
+} 
+
+$Disks | ForEach-Object {
+    
+    if($_.'Free(%)' -le $WarningLevel -and $_.'Free(%)' -gt $CriticalLevel){
+        if($Nagios -ne 2){
+            $Nagios = 1
+            $Notify = 'WARNING'
+            }
+        }
+    if($_.'Free(%)' -le $CriticalLevel){
+        $Nagios = 2
+        $Notify = 'CRITICAL'
+    }
+}
+
+Write-Host 'RDS Users Profile Space is at' $Notify 'Levels'
+$Disks | Where-Object {$_.'Free(%)' -le $WarningLevel}| Sort-Object 'Free(%)' | Format-Table Name, 'Capacity(GB)', 'FreeSpace(GB)', 'Free(%)' -AutoSize
+
+$Nagios
